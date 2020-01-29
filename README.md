f180c3a8097c7f679cf7bcf1f47464bb64fe08c5
Dave Horton <daveh@beachdognet.com>
2020-01-29 13:14:20 -0500

    initial checkin

---
+++ README.md
@@ -0,0 +1,44 @@
+# drachtio-http-authenticator
+
+drachtio middleware that delegates sip authentication to an http api.  This allows, for instance, a multi-tenant sip application server to delegate authentication to a customer api.
+
+The middleware-returning function can be invoked EITHER with an HTTP URL to call OR a function yielding a Promise that resolves to an HTTP URL.  Alternatively, instead of a returning a URL, and object can be provided with `url`, `username`, and `password` properties if you wish to protect your endpoint with HTTP Basic Authentication. 
+
+An HTTP POST will be made to the specified URL with a JSON body containing the sip method and the components from the Authorization header.  The HTTP server should return a status code of 200 in all cases, containing a JSON body with instructions on whether to admit the request.
+
+To admit the request, send a 200 response with a `status` of `ok`, e.g.
+```
+{"status": "ok"}
+```
+To deny the request, send a 200 response with a `status` of `fail`.  The `status` field MUST be provided.  Optionally, a response MAY include a `msg` attribute, an `expires` attribute, and/or a `blacklist` attribute.  
+
+- The `msg` property is simply a human-readable description of why an authentication failed.
+- The `expires` value provides a value in seconds for the duration of a granted registration.  This value, if provided, must be less than the requested expiration. If not provided, the requested expires value is granted.
+- The `blacklist` property shall contain a number indicating a period of time, in seconds, that the source IP address should be blocked.  A value of -1 means forever.
+```
+{"status": "fail"}
+```
+or
+```
+{"status": "fail", "msg": "unknown user"}
+```
+```
+{"status": "fail", "blaclist": 3600}
+```
+
+
+Additionally, for admitted requests, the middleware adds a `req.authorization` object which contains two properties:
+- challengeResponse - an object containing the parsed elements of the sip Authorization header, and
+- grant - an object containing the json response received in the 200 OK to the POST request.
+
+```
+const authenticator = require('drachtio-http-authenticator')({
+  url: 'https://example.com/auth',
+  auth: {
+    username: 'foo',
+    password: 'bar'
+  }
+});
+
+srf.use('invite', authenticator);
+```
---
+++ app.js
@@ -0,0 +1,131 @@
+const request = require('request');
+const nonce = require('nonce')();
+const parseUri = require('drachtio-srf').parseUri;
+const debug = require('debug')('drachtio:http-authenticator');
+
+function parseAuthHeader(hdrValue) {
+  const pieces = { scheme: 'digest'} ;
+  ['username', 'realm', 'nonce', 'uri', 'algorithm', 'response', 'qop', 'nc', 'cnonce', 'opaque']
+    .forEach((tok) => {
+      const re = new RegExp(`[,\\s]{1}${tok}="?(.+?)[",]`) ;
+      const arr = re.exec(hdrValue) ;
+      if (arr) {
+        pieces[tok] = arr[1];
+        if (pieces[tok] && pieces[tok] === '"') pieces[tok] = '';
+      }
+    }) ;
+
+  pieces.algorithm = pieces.algorithm || 'MD5' ;
+
+  // this is kind of lame...nc= (or qop=) at the end fails the regex above,
+  // should figure out how to fix that
+  if (!pieces.nc && /nc=/.test(hdrValue)) {
+    const arr = /nc=(.*)$/.exec(hdrValue) ;
+    if (arr) {
+      pieces.nc = arr[1];
+    }
+  }
+  if (!pieces.qop && /qop=/.test(hdrValue)) {
+    const arr = /qop=(.*)$/.exec(hdrValue) ;
+    if (arr) {
+      pieces.qop = arr[1];
+    }
+  }
+
+  // check mandatory fields
+  ['username', 'realm', 'nonce', 'uri', 'response'].forEach((tok) => {
+    if (!pieces[tok]) throw new Error(`missing authorization component: ${tok}`);
+  }) ;
+  debug(`parsed header: ${JSON.stringify(pieces)}`);
+  return pieces ;
+}
+
+function respondChallenge(req, res) {
+  const nonceValue = nonce();
+  const uri = parseUri(req.uri);
+  const headers = {
+    'WWW-Authenticate': `Digest realm="${uri.host}", algorithm=MD5, qop="auth", nonce="${nonceValue}"`
+  };
+  debug('sending a 401 challenge');
+  res.send(401, {headers});
+}
+
+function digestChallenge(obj, logger) {
+  let dynamicCallback;
+  if (!logger) logger = {info: () => {}, error: () => {}};
+  if (typeof obj === 'string') obj = {uri: obj};
+  else if (typeof obj === 'function') dynamicCallback = obj;
+
+  return async(req, res, next) => {
+    let auth, uri, qs, body;
+    let method = 'POST';
+
+    // challenge requests without credentials
+    if (!req.has('Authorization')) return respondChallenge(req, res);
+
+    const pieces = parseAuthHeader(req.get('Authorization'));
+    const expires = req.registration ? req.registration.expires : null;
+    const data = Object.assign({method: req.method, expires}, pieces);
+
+    if (dynamicCallback) {
+      const sipUri = parseUri(req.uri);
+      try {
+        const obj = await dynamicCallback(sipUri.host);
+        if (!obj) {
+          logger.debug(`jambonz-http-authenticator: Unknown realm ${sipUri.host}, rejecting with 403`);
+          return res.send(403);
+        }
+        logger.debug({obj}`jambonz-http-authenticator realm ${sipUri.host} auth details`);
+        if (typeof obj === 'object') {
+          uri = obj.uri || obj.url;
+          if (obj.auth) auth = Object.assign({}, obj.auth);
+          if (obj.method) method = obj.method.toUpperCase();
+        }
+        else uri = obj;
+      } catch (err) {
+        logger.error(`Error ${err}, rejecting with 403`);
+        return next(err);
+      }
+    }
+    else {
+      uri = obj.uri || obj.url;
+      if (obj.auth) auth = Object.assign({}, obj.auth);
+    }
+
+
+    debug(`parsed authorization header: ${JSON.stringify(pieces)}`);
+    const opts = Object.assign({
+      uri,
+      auth,
+      method,
+      json: true
+    });
+    if ('GET' === method) qs = data;
+    else body = data;
+
+    request(opts, (err, response, body) => {
+      if (err) {
+        debug(`Error from calling auth callback: ${err}`);
+        return next(err);
+      }
+      debug(`received ${response.statusCode} with body ${JSON.stringify(body)}`);
+      if (response.statusCode !== 200) {
+        debug(`auth callback returned a non-success response: ${response.statusCode}`);
+        return res.send(500);
+      }
+      if (body.status != 'ok') {
+        // TODO: deal with blacklist requests
+        res.send(403);
+      }
+
+      // success
+      req.authorization = {
+        challengeResponse: pieces,
+        grant: body
+      };
+      next();
+    });
+  };
+}
+
+module.exports = digestChallenge;
